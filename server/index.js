e704be0ecb6ba90c8d044cc9e13210036095b4f5
Solomon-Asefa <soloasefa6603@gmail.com>
2025-03-11 11:04:12 +0300

    fix webhook

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
 console.log("MONGO_URL: " + MONGO_URL);
 
-app.use("/", clerkWebhooks);
+app.use("/webhook", clerkWebhooks);
 
 app.use(errorMiddleware);
 // Basic route
